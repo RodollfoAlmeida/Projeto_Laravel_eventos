8223225eceb814ac208a48fa90e90bd6eb59626c
Rodolfo Almeida <rodolfogaspardealmeida@gmail.com>
2026-04-14 23:23:33 -0300

    criação de tela de para visualizar os detealhes do evento

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,12 +14,12 @@
         <div id="cards-container" class="row">
           @foreach ($events as $event)
           <div class="card col-md-3">
-            <img src="/img/event_placeholder.jpg" alt="{{ $event->title}}">
+            <img src="/img/events/{{ $event->image }}" alt="{{ $event->title}}">
             <div class="card-body">
                 <p class="card-date">06/04/2026</p>
                 <h5 class="card-title">{{ $event->title }}</h5>
                 <p class="card-participantes">X Participantes</p>
-                <a href="#" class="btn btn-primary">Saber Mais</a>
+                <a href="/events/{{ event->$id }}" class="btn btn-primary">Saber Mais</a>
             </div>
           </div>
               
